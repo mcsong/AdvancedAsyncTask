dc5fbddc00682073fc6d6433f25c264a4dc86da2
Justin(Moo Chan) Song <mcsong@gmail.com>
2016-08-18 16:43:31 -0700

    Upates build scripts

---
+++ dist_library_bintray.bat
@@ -0,0 +1,1 @@
+gradlew :library:clean :library:build :library:bintrayUpload -PbintrayUser=mcsong -PbintrayKey=604470e19d2259e8d841e02f8b5c633ec9cc7614 -PdryRun=false --info --stacktrace
---
+++ dist_library_check.bat
@@ -0,0 +1,1 @@
+gradlew :library:clean :library:build :library:bintrayUpload -PbintrayUser=mcsong@gmail.com -PbintrayKey=604470e19d2259e8d841e02f8b5c633ec9cc7614 -PdryRun=true --info --stacktrace
